bf555bc20104a44b86df47d11d6d2bb6f95c6ce6
Adam McKellar <dev@mckellar.eu>
2025-03-11 05:12:47 +0100

    fix: name of webpage, maybe even deployment

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginCssMinimizer } from '@rsbuild/plugin-css-minimizer';
 
 export default defineConfig({
+  html: {
+    title: 'Fibonacci Toy Website',
+  },
   plugins: [pluginCssMinimizer()],
   tools: {
     lightningcssLoader: false,
+  },
+  output: {
+    assetPrefix: './static/',
   },
 });
