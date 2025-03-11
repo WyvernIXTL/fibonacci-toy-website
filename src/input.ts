260725c0ea4e28ae3a046235bc526f549b724fd4
Adam McKellar <dev@mckellar.eu>
2025-03-11 02:07:43 +0100

    default to rust wasm

--- src/input.ts
+++ src/input.ts
@@ -20,7 +20,7 @@ export function defaultIntInputState(): IntInputState {
     raw: '',
     int: undefined,
     valid: true,
-    algorithm: FibonacciAlgorithm.Linear,
+    algorithm: FibonacciAlgorithm.LinearRs,
   };
 }
 
