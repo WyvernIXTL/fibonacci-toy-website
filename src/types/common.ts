9abe91701b9297e611c52a1bc6cd94db775c2c3c
Adam McKellar <dev@mckellar.eu>
2025-03-13 21:57:08 +0100

    feat: added abstraction for props

--- src/types/common.ts
+++ src/types/common.ts
@@ -4,27 +4,62 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+import type { VNode } from 'hyperapp';
+
+/**
+ * A getter takes a state and returns parts of it.
+ *
+ * @param state A higher state.
+ * @returns A lower state that is withing the higher state.
+ */
+export type Getter<State, SubState> = (state: State) => SubState;
+
+/**
+ * A setter takes a state ({@link state}), copies it, modifies parts of the copy with {@link newSubState} and then returns the copy.
+ *
+ * It is important that the input object is not the output object!
+ * Hyperapp will not register a change of state if the objects match.
+ *
+ * @param state A higher state that holds the state we wish to replace.
+ * @param newSubState The new state that is within {@link state}.
+ * @returns New higher state.
+ */
+export type Setter<State, SubState> = (
+  state: State,
+  newSubState: SubState,
+) => State;
+
 /**
  * Abstraction to help with setter and getter for accessing structured states.
  */
-export interface StateGetterSetter<T, L> {
-  /**
-   * A getter takes a state and returns parts of it.
-   *
-   * @param appState A higher state.
-   * @returns A lower state that is withing the higher state.
-   */
-  getter: (appState: T) => L;
+export interface StateGetterSetter<State, SubState> {
+  getter: Getter<State, SubState>;
+  setter: Setter<State, SubState>;
+}
+
+export abstract class Component<State, SubState> {
+  private readonly set: Setter<State, SubState>;
+  private readonly get: Getter<State, SubState>;
+
+  constructor(stateGetterSetter: StateGetterSetter<State, SubState>) {
+    this.get = stateGetterSetter.getter;
+    this.set = stateGetterSetter.setter;
+  }
 
   /**
-   * A setter takes a state ({@link appState}), copies it, modifies parts of the copy with {@link value} and then returns the copy.
+   * Renders the component via its state.
    *
-   * It is important that the input object is not the output object!
-   * Hyperapp will not register a change of state if the objects match.
-   *
-   * @param appState A higher state that holds the state we wish to replace.
-   * @param value The new state that is within {@link appState}.
-   * @returns New higher state.
+   * @param state State of this component.
    */
-  setter: (appState: T, value: L) => T;
+  abstract render(state: SubState): VNode<State>;
+
+  /**
+   * Render component with app state.
+   *
+   * @param state State of app.
+   * @returns Rendered component.
+   */
+  public view(state: State): VNode<State> {
+    return this.render(this.get(state));
+  }
 }
