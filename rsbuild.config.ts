6864563c8f3d30f8cb8965ed1f43fceb398ea13c
Adam McKellar <dev@mckellar.eu>
2025-03-23 13:59:33 +0100

    removed: beercss and cssnano

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,30 +1,21 @@
 import { defineConfig } from '@rsbuild/core';
-import { pluginCssMinimizer } from '@rsbuild/plugin-css-minimizer';
 
 // biome-ignore lint/style/noDefaultExport: <explanation>
 export default defineConfig({
   html: {
     template: './src/index.html',
     title: 'Fibonacci Toy Website',
-    inject: 'body',
+    // inject: 'body',
     meta: {
       description: 'Calculate fibonacci numbers, fast, on the web!',
     },
   },
-  plugins: [
-    pluginCssMinimizer({
-      pluginOptions: {
-        minimizerOptions: {
-          preset: require.resolve('cssnano-preset-advanced'),
-        },
-      },
-    }),
-  ],
+  plugins: [],
   tools: {
-    lightningcssLoader: false,
+    lightningcssLoader: true,
   },
   output: {
-    inlineScripts: true,
-    inlineStyles: true,
+    // inlineScripts: true,
+    // inlineStyles: true,
   },
 });
